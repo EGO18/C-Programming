6900b1fbb5520e983e905dd402ea127f638bcf99
Evan Owens <eowens9516@floridapoly.edu>
2025-08-20 18:46:01 +0000

    try

---
+++ 01_c++_intro/first_program.cpp
@@ -0,0 +1,19 @@
+/*
+20/08/2025
+The first C++ program
+*/
+
+#include <iostream>
+
+// Create a main function
+int main(void)
+{
+    std::cout << "Hello COP3337!" << std::endl;
+
+    // std - standard namespace
+    // :: - scope resolution operator
+    // cout - console output (object of an iostream class)
+    //<< - stream insertion operator
+    // end - end of line
+    return 0;
+}
