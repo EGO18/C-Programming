aa7a0e7aa12c6c3efa05d95dc837610d8b6f7fd7
Evan Owens <eowens9516@floridapoly.edu>
2025-10-10 18:49:44 +0000

    Hello

---
+++ 15_hello_class/hello.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Hello
+{
+    public:
+        Hello()
+        {
+           std::cout << "Constructor" << std::endl;    
+        }
+        Hello(int n)
+        {
+            std::cout << "Constructor with arguments" << std::endl;
+        }
+        void bye(void)
+        {
+            std::cout << "Bye!" << std::endl;
+        }
+};
+
+int main()
+{
+    //Create an object dynamically
+    Hello * hi = new Hello;
+
+    //Create an object dynamically
+    Hello * hi2 = new Hello(10);
+
+    //Access bye method
+    hi -> bye();
+
+    // Deallocate the memory
+    delete hi;
+    delete hi2;
+
+    return 0;
+}
