81579d6d3cf91d316080ee2bdc833ffea1f7c753
Evan Owens <eowens9516@floridapoly.edu>
2025-10-15 17:50:23 +0000

    MyVector

---
+++ 16_My_Vector/main.cpp
@@ -0,0 +1,26 @@
+#include "MyVector.hpp"
+
+int main()
+{
+    // Create a MyVector object dynamically
+    MyVector *mvPtr = new MyVector(10);
+    
+
+    mvPtr ->push_back(1);
+    mvPtr ->push_back(2);
+    mvPtr ->push_back(3);
+    mvPtr ->push_back(4);
+    mvPtr ->push_back(1);
+    mvPtr ->push_back(2);
+    mvPtr ->push_back(3);
+    mvPtr ->push_back(4);
+    mvPtr ->push_back(1);
+    mvPtr ->push_back(2);
+    mvPtr ->push_back(3);
+    mvPtr ->push_back(4);
+
+    mvPtr->print();
+
+    delete mvPtr;
+    return 0;
+}
